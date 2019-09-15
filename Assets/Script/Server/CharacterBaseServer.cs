9ef32d23326ce282e336c3ad4ee658f7e77477ef
Godole <godole199@naver.com>
2019-09-16 01:03:04 +0900

    AddNetwork2
    네트워크 추가 한번 더함

---
+++ Assets/Script/Server/CharacterBaseServer.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Utility;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityStandardAssets.Characters.FirstPerson;
+
+[RequireComponent(typeof(CharacterController))]
+public class CharacterBaseServer : MonoBehaviour
+{
+    public float m_WalkSpeed;
+    public float m_TurnSpeed;
+
+    private MouseLook m_MouseLook;
+    private Camera m_Camera;
+    private CharacterController m_CharacterController;
+    private Vector3 m_MoveDir = Vector3.zero;
+
+    private float m_TurnAmount = 0.0f;
+    private float m_ForwardAmount = 0.0f;
+    private float m_MovingTurnSpeed = 360;
+    private float m_StationaryTurnSpeed = 180;
+
+    void Start()
+    {
+        m_MouseLook = new MouseLook();
+        m_Camera = Camera.main;
+        m_CharacterController = GetComponent<CharacterController>();
+        m_MouseLook.Init(transform, m_Camera.transform);
+    }
+
+    public void Move(float horizontal, float vertical)
+    {
+        Vector3 input = new Vector3(horizontal, vertical);
+        Vector3 desiredMove = m_Camera.transform.forward * input.y + m_Camera.transform.right * input.x;
+
+        RaycastHit hitInfo;
+        Physics.SphereCast(transform.position, m_CharacterController.radius, Vector3.down, out hitInfo,
+                           m_CharacterController.height / 2f, Physics.AllLayers, QueryTriggerInteraction.Ignore);
+        desiredMove = Vector3.ProjectOnPlane(desiredMove, hitInfo.normal).normalized;
+
+        m_MoveDir.x = desiredMove.x * m_WalkSpeed;
+        m_MoveDir.z = desiredMove.z * m_WalkSpeed;
+
+        m_CharacterController.Move(m_MoveDir * Time.fixedDeltaTime);
+
+        Rotation(horizontal, vertical);
+    }
+
+    void Rotation(float h, float v)
+    {
+        if(h != 0 || v != 0)
+        {
+            Vector3 camForward = m_Camera.transform.forward;
+            camForward = new Vector3(camForward.x, 0, camForward.z);
+            camForward.Normalize();
+
+            float signedangle = Vector3.SignedAngle(Vector3.forward, camForward, Vector3.up);
+
+            Quaternion rotateAngle = Quaternion.Euler(0, signedangle, 0);
+            Vector3 targetAngle = rotateAngle * new Vector3(h, 0, v);
+
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(targetAngle), m_TurnSpeed * Time.deltaTime);
+        }
+    }
+
+    public virtual void Attack()
+    {
+
+    }
+}
---
+++ Assets/Script/Server/UserInputServer.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine.UI;
+
+public class UserInputServer : MonoBehaviourPunCallbacks,IPunObservable
+{
+    public CharacterBaseServer m_Client;
+    public PhotonView PV;
+    public Text NickNameText;
+    public ThirdPersonCameraServer TPC;
+
+    Vector3 curPos;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        PV = FindObjectOfType<PhotonView>();
+        //닉네임
+        NickNameText.text = PV.IsMine ? PhotonNetwork.NickName : PV.Owner.NickName;
+        //포톤뷰가 내꺼면 내이름 넣고 아니면 그 포톤뷰 주인의 이름을 넣음
+        NickNameText.color = PV.IsMine ? Color.green : Color.red;
+        //마찬가지로 포톤뷰가 내꺼면 초록색 아니면 빨간색.
+        TPC = FindObjectOfType<ThirdPersonCameraServer>();
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            m_Client.Attack();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (PV.IsMine)
+        {
+            float horizontal = CrossPlatformInputManager.GetAxis("Horizontal");
+            float vertical = CrossPlatformInputManager.GetAxis("Vertical");
+
+            m_Client.Move(horizontal, vertical);
+            TPC.m_LookAt = m_Client.transform;
+        }
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+    }
+}
