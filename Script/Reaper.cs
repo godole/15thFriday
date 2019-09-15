09e22cbc179aa1d94fe99bfb434c7c4601aba7fe
Godole <godole199@naver.com>
2019-09-16 00:44:45 +0900

    AddNetwork
    네트워크 추가

---
+++ Script/Reaper.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reaper : CharacterBase
+{
+    public Vector3 m_AttackCenter;
+    public Vector3 m_AttackSize;
+
+    public override void Attack()
+    {
+        var obj = Physics.OverlapBox(m_AttackCenter + transform.position, m_AttackSize / 2, new Quaternion(), 1 << 10);
+
+        foreach(var o in obj)
+        {
+            o.gameObject.GetComponent<Ghost>().Hit();
+        }
+    }
+
+}
---
+++ Script/UserInput.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class UserInput : MonoBehaviour
+{
+    public CharacterBase m_Client;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            m_Client.Attack();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        float horizontal = CrossPlatformInputManager.GetAxis("Horizontal");
+        float vertical = CrossPlatformInputManager.GetAxis("Vertical");
+
+        m_Client.Move(horizontal, vertical);
+    }
+}
