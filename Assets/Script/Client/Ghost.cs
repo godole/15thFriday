4da0a0cd88e62a5821c81b43cf47302985d376f1
Godole <godole199@naver.com>
2019-09-17 02:58:35 +0900

    AddItemAndGate
    아이템이랑 관문 만듦

--- Assets/Script/Client/Ghost.cs
+++ Assets/Script/Client/Ghost.cs
@@ -6,10 +6,29 @@ using UnityStandardAssets.Characters.FirstPerson;
 public class Ghost : CharacterBase
 {
     public float m_TurnSpeed;
+    public GameObject m_EnergyBalls;
+
+    GameObject m_RangedEnergyBall;
+    private int m_EnergyBallCount = 0;
+
+    public int EnergyBallCount { get => m_EnergyBallCount; set => m_EnergyBallCount = value; }
+
+    protected override void Init()
+    {
+        base.Init();
+    }
 
     public void Hit()
     {
         Debug.Log("Hit");
+    }
+
+    private void Update()
+    {
+        if(Input.GetButtonDown("Fire2"))
+        {
+            EatEnergyBall();
+        }
     }
 
     public override void Attack()
@@ -39,5 +58,38 @@ public class Ghost : CharacterBase
 
             transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(targetAngle), m_TurnSpeed * Time.deltaTime);
         }
+    }
+
+    public void RangedEnergyBall(GameObject ball)
+    {
+        m_RangedEnergyBall = ball;
+    }
+
+    public void OutOfRangeBall()
+    {
+        m_RangedEnergyBall = null;
+    }
+
+    public void EatEnergyBall()
+    {
+        if(m_RangedEnergyBall != null)
+        {
+            m_RangedEnergyBall.transform.parent = m_EnergyBalls.transform;
+            m_RangedEnergyBall.transform.position = transform.position + new Vector3(0, 1 + m_EnergyBallCount, 0);
+            m_RangedEnergyBall.transform.rotation = new Quaternion();
+            m_RangedEnergyBall.GetComponent<EnergyBall>().m_EffectObj.SetActive(false);
+            m_EnergyBallCount++;
+        }
+    }
+
+    public void EnterGate()
+    {
+        m_EnergyBallCount = 0;
+        Destroy(m_EnergyBalls);
+
+        m_EnergyBalls = new GameObject();
+        m_EnergyBalls.transform.parent = transform;
+        m_EnergyBalls.transform.position = new Vector3();
+        m_EnergyBalls.transform.rotation = new Quaternion();
     }
 }
